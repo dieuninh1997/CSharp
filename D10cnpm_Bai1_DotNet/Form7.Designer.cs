244f15177e8f73245aad36834e4430fdc11879e3
dieuninh1997 <ttdn1997@gmail.com>
2017-09-21 00:03:17 +0700

    update bai7

--- D10cnpm_Bai1_DotNet/Form7.Designer.cs
+++ D10cnpm_Bai1_DotNet/Form7.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvSearch = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearch)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(85, 32);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(462, 22);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tìm kiếm";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(553, 31);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvSearch
+            // 
+            this.dgvSearch.AllowUserToAddRows = false;
+            this.dgvSearch.AllowUserToDeleteRows = false;
+            this.dgvSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSearch.Location = new System.Drawing.Point(19, 86);
+            this.dgvSearch.Name = "dgvSearch";
+            this.dgvSearch.ReadOnly = true;
+            this.dgvSearch.Size = new System.Drawing.Size(609, 241);
+            this.dgvSearch.TabIndex = 3;
             // 
             // Form7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(773, 271);
+            this.ClientSize = new System.Drawing.Size(648, 353);
+            this.Controls.Add(this.dgvSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
             this.Name = "Form7";
-            this.Text = "Form7";
+            this.Text = "Tìm kiếm";
             this.Load += new System.EventHandler(this.Form7_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearch)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvSearch;
     }
 }
--- D10cnpm_Bai1_DotNet/Form7.cs
+++ D10cnpm_Bai1_DotNet/Form7.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Data.SqlClient;
+
 namespace D10cnpm_Bai1_DotNet
 {
     public partial class Form7 : Form
@@ -19,32 +21,47 @@ namespace D10cnpm_Bai1_DotNet
 
         private void Form7_Load(object sender, EventArgs e)
         {
-
+            txtSearch.Text = "";
+            txtSearch.Focus();
         }
-        public static string ConvertToUnSign(string text)
+        public static string convertToUnSign3(string s)
         {
-            for (int i = 33; i < 48; i++)
-            {
-                text = text.Replace(((char)i).ToString(), "");
-            }
+            Regex regex = new Regex("\\p{IsCombiningDiacriticalMarks}+");
+            string temp = s.Normalize(NormalizationForm.FormD);
+            return regex.Replace(temp, String.Empty).Replace('\u0111', 'd').Replace('\u0110', 'D');
+        }
 
-            for (int i = 58; i < 65; i++)
-            {
-                text = text.Replace(((char)i).ToString(), "");
-            }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
 
-            for (int i = 91; i < 97; i++)
+            String chuoi = txtSearch.Text.ToString();
+            MessageBox.Show(convertToUnSign3(chuoi));
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=DIEUNINH\\SQLEXPRESS;Initial Catalog=Bai1DotNet;Integrated Security=True";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("sp_Search", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@chuoi", convertToUnSign3(chuoi)));
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            //duyet qua tat ca cac cot trong reader
+            for (int i=0; i<reader.FieldCount; i++)
             {
-                text = text.Replace(((char)i).ToString(), "");
+                table.Columns.Add(i.ToString());
             }
-            for (int i = 123; i < 127; i++)
+            //duyet qua toan bo cac dong reader
+            while(reader.Read())
             {
-                text = text.Replace(((char)i).ToString(), "");
+                object[] arr = new object[reader.FieldCount];
+                reader.GetValues(arr);
+                table.Rows.Add(arr);
             }
-            text = text.Replace(" ", "-");
-            Regex regex = new Regex(@"\p{IsCombiningDiacriticalMarks}+");
-            string strFormD = text.Normalize(System.Text.NormalizationForm.FormD);
-            return regex.Replace(strFormD, String.Empty).Replace('\u0111', 'd').Replace('\u0110', 'D');
+            table.Columns[0].ColumnName = "Tài khoản";
+            table.Columns[1].ColumnName = "Mật khẩu";
+            dgvSearch.DataSource = table;
+            
+            conn.Close();
         }
     }
 }
